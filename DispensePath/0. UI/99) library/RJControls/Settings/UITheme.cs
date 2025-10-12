a668b6db6a54911e675fceab805af1b762e82022
KangSeungJoo <trazomssome@gmail.com>
2025-10-12 23:18:10 +0900

    DispenserPath 제거

--- DispensePath/0. UI/99) library/RJControls/Settings/UITheme.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace IF_UI.Settings
-{
-    public enum UITheme
-    {
-        // Define the application themes.
-        Light, // The background color of the application is light.
-        Dark // The background color of the application is dark.
-    }
-}
